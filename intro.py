c2b5b20bb5bcb3b2094d7363dfa6dc67934aa8cd
IsiteYves <yvesisite@gmail.com>
2022-10-18 20:51:58 +0200

    feat: if statements pounds-to-kilos converter and vice versa

--- intro.py
+++ intro.py
@@ -1,2 +1,21 @@
-name = 'jennifer'
-print(name.find('e'))
+# isHot=False
+# is_cold=False
+# if isHot:
+#     print("It's a hot day")
+# elif is_cold and not isHot:
+#     print("It's a coldyyy day")
+# else:
+#     print("It's a fair day!!")
+# print('Enjoy!!')
+def convertWeight(weight, typeOfWeight):
+    if typeOfWeight == 'K':
+        return weight / 0.45
+    else:
+        return weight * 0.45
+weight=int(input('Weight: '))
+typeOfWeight=input('(L)bs or (K)g: ')
+typeOfWeight=typeOfWeight.upper()
+if typeOfWeight == 'L':
+    print(f"You are {convertWeight(weight, 'L')} kilos")
+else:
+    print(f"You are {convertWeight(weight, 'K')} pounds")

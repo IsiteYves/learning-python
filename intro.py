c36e58e8cfa95865a1ce7bc5b653b70b466b0f45
IsiteYves <yvesisite@gmail.com>
2022-10-19 15:12:58 +0200

    feat: use dictionary to print phone in words

--- intro.py
+++ intro.py
@@ -64,4 +64,4 @@
 phone=list(input('Phone: '))
 words={'1': 'One', '2': 'Two', '3': 'Three', '4': 'Four', '5': 'Five', '6': 'Six', '7': 'Seven', '8': 'Eight', '9': 'Nine', '0': 'Zero'}
 for nbr in phone:
-    print(words[nbr], end=' ')
+    print(words.get(nbr, '!'), end=' ')

6dd273f5cc762468dca67e977f5960be65ae0c81
IsiteYves <yvesisite@gmail.com>
2022-10-21 07:20:56 +0200

    feat: random dice roller

--- impl.py
+++ impl.py
@@ -1,3 +1,13 @@
-from ecommerce import shipping
+import random
 
-shipping.calc_shipping()
+choice = 2
+print('''
+0) Exit
+1) Roll again
+      ''')
+while choice != 0:
+    choice = int(input('>'))
+    if choice ==  0:
+        break
+    else:
+        print(f'({random.randint(1, 6)}, {random.randint(1, 6)})')

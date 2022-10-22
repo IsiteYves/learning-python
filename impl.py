672a34ada6004558339e4c233f2a3a62e0b0b391
IsiteYves <yvesisite@gmail.com>
2022-10-22 05:45:35 +0200

    feat: updating data in excel using openpyxl and chart generating

--- impl.py
+++ impl.py
@@ -1,13 +1,23 @@
-import random
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
-choice = 2
-print('''
-0) Exit
-1) Roll again
-      ''')
-while choice != 0:
-    choice = int(input('>'))
-    if choice ==  0:
-        break
-    else:
-        print(f'({random.randint(1, 6)}, {random.randint(1, 6)})')
+XLfile=xl.load_workbook('salaries.xlsx')
+
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    correct_val = cell.value * 0.814
+    sheet.cell(row, 4).value = correct_val
+    
+content = Reference(sheet, min_row=2,
+                   max_row=sheet.max_row,
+                   min_col=4,
+                   max_col=4)
+
+chart=BarChart()
+chart.add_data(content)
+sheet.add_chart(chart, 'f5')
+    
+XLfile.save('salaries.xlsx')
+
+
+
